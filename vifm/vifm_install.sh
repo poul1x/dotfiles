179b017c909ba015d9c3835ff4327dc9e0732ae6
Pavel Knyazev <poulix.nova@mail.ru>
2021-01-02 17:24:29 +0300

    Add vifm config

---
+++ vifm/vifm_install.sh
@@ -0,0 +1,4 @@
+sudo apt install -y vifm
+mkdir -p ~/.vifm
+cp ./vifmrc ~/.vifm/vifmrc
+wget -P ~/.vifm/colors https://raw.githubusercontent.com/vifm/vifm-colors/master/gruvbox.vifm
---
+++ vifm/vifm_uninstall.sh
@@ -0,0 +1,2 @@
+sudo apt remove vifm
+rm -rf ~/.vifm
