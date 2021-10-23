468bf49af1f57bea0e1f1aa811d8a0c87e4a6137
Pavel Knyazev <poulix.nova@mail.ru>
2021-10-23 20:17:44 +0300

    Update k8s client install script

--- k8s/k8scli_install.sh
+++ k8s/k8scli_install.sh
@@ -10,9 +10,11 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 # And extension neat (cleaning yaml files)
 (
   set -x; cd "$(mktemp -d)" &&
-  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz" &&
-  tar zxvf krew.tar.gz &&
-  KREW=./krew-"$(uname | tr '[:upper:]' '[:lower:]')_$(uname -m | sed -e 's/x86_64/amd64/' -e 's/arm.*$/arm/')" &&
-  "$KREW" install krew
-  "$KREW" install neat
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+  ./"${KREW}" install neat
 )
